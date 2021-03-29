313e7967cbecbcf3aa9f4d29109256fb02c6f110
subhashishaich <76804284+subhashishaich@users.noreply.github.com>
2021-03-29 12:07:15 +0530

    Final Commit
    Changes tested for password strength

---
+++ imageHoster/src/main/java/ImageHoster/service/UserService.java
@@ -0,0 +1,57 @@
+package ImageHoster.service;
+
+import ImageHoster.model.User;
+import ImageHoster.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    //Call the registerUser() method in the UserRepository class to persist the user record in the database
+    public void registerUser(User newUser) {
+        userRepository.registerUser(newUser);
+    }
+
+    //Since we did not have any user in the database, therefore the user with username 'upgrad' and password 'password' was hard-coded
+    //This method returned true if the username was 'upgrad' and password is 'password'
+    //But now let us change the implementation of this method
+    //This method receives the User type object
+    //Calls the checkUser() method in the Repository passing the username and password which checks the username and password in the database
+    //The Repository returns User type object if user with entered username and password exists in the database
+    //Else returns null
+    public User login(User user) {
+        User existingUser = userRepository.checkUser(user.getUsername(), user.getPassword());
+        if (existingUser != null) {
+            return existingUser;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * This method check if the password strength is valid.
+     * Password strength is valid if it contains one digit , one alphabet and one special character.
+     *
+     * @param password
+     * @return Returns true if the password has expected strength else false.
+     */
+    public boolean isPasswordStrengthValid(String password) {
+        System.out.println(password);
+        if (!password.isEmpty() && password.length() >= 3) {
+            String regex = "^(?=.*[a-z])(?=." + "*[A-Z])(?=.*\\d)" + "(?=.*[-+_!@#$%^&*., ?]).+$";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(password);
+            if (matcher.matches()) return true;
+            else return false;
+        }
+        return false;
+    }
+}
+
